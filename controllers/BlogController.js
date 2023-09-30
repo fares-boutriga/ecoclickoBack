cf6370b04f35f31f2f6b0961a97973bdb44a12ed
fares-boutriga <117439484+fares-boutriga@users.noreply.github.com>
2023-09-30 21:34:50 +0100

    second comment

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,1 +1,24 @@
-console.log("object")
+const Blog =require('../database/models/BlogModel')
+
+module.exports={
+    createClaim : (req , res) => {
+        const BlogTitle = req.body.blogTitle;
+        const BlogContent = req.body.blogContent;
+        const BlogImage = req.body.blogImage;
+        const CommnetID=req.params.commentId
+         Blog.create({BlogTitle,BlogContent,BlogImage,CommnetID})
+         .then(result => {
+           res.status(201).send(result);
+         })
+         .catch((err) => {
+           res.status(500).send(err);
+         });
+         
+      },
+      getAllBlog:(req,res)=>{
+        Blog.findAll()
+        .then(result=>{
+            res.status(201).send(result)
+        })
+      }
+}
---
+++ database/models/CommentsModel.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize =require('../configdb')
+
+const Comments = sequelize.define('Comments', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    like: {
+      type: DataTypes.TINYINT,
+      allowNull: true,
+    },
+  });
+  Comments.hasMany(Users, { foreignKey: 'Comments_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
